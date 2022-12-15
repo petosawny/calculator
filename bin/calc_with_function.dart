79f539fd4245786efef08be5934c0c5d9718f875
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 05:03:15 +0200

    Update calc_with_function.dart

--- bin/calc_with_function.dart
+++ bin/calc_with_function.dart
@@ -1,61 +1,100 @@
+// import packages and classes
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print("enter the first number");
-  double? numb1 = double.parse(stdin.readLineSync()!);
-  print("enter the second number");
-  double? numb2 = double.parse(stdin.readLineSync()!);
+  print("enter the first number"); // print statement to the user
+  double? numb1 = double.parse(
+      stdin.readLineSync()!); // insert value to the variable op from user
+
+  print("enter the second number"); // print statement to the user
+  double? numb2 = double.parse(
+      stdin.readLineSync()!); // insert value to the variable op from user
+  // print statement show to the user the operations
   print(''''enter the operation
   if you want to Plus pres number       + 
   if you want to Subtract pres number   - 
   if you want to Multiply pres number   * 
  if you want to Devide pres number     / ''');
-  String? operator = stdin.readLineSync();
+  String? operator =
+      stdin.readLineSync(); // insert value to the variable op from user
+// calling void fun with 3 parameters
   calculate(numb1, numb2, operator!);
-  double newResult = calc(numb1, numb2, operator) * 2;
-  print(" the NewResult is");
-  print(newResult);
+  double newResult = calc(numb1, numb2, operator) *
+      2; // define a variable and the result is the calling of a double fun with 3 parameters
+  print(" the NewResult is"); // print statement to the user
+  print(newResult); // print the var to the user
 }
 
+// creating a void fun with 3 parameters
 void calculate(double numb1, double numb2, String operator) {
   if (operator == "+") {
-    print(numb1 + numb2);
+    // if statement to comparing the operation which the user need and calculate the operation with the relevant operation
+
+    print(numb1 + numb2); // print the result of the operation to the user
   } else if (operator == "-") {
-    print(numb1 - numb2);
+    // if statement to comparing the operation which the user need and calculate the operation with the relevant operation
+
+    print(numb1 - numb2); // print the result of the operation to the user
   } else if (operator == "*") {
-    print(numb1 * numb2);
+    // if statement to comparing the operation which the user need and calculate the operation with the relevant class
+    print(numb1 * numb2); // print the result of the operation to the user
   } else if (operator == "/") {
-    print(numb1 / numb2);
+    // if statement to comparing the operation which the user need and calculate the operation with the relevant class
+    print(numb1 / numb2); // print the result of the operation to the user
     if (numb2 == 0) {
-      print("the second number should be a natural number");
-      print("enter the second number again");
-      int? numb2 = int.parse(stdin.readLineSync()!);
-      print(numb1 / numb2);
+      // conditional statement to avoid divided by zero
+      print(
+          "the second number should be a natural number"); // print statement to the user
+      print("enter the second number again"); // print statement to the user
+      int? numb2 = int.parse(stdin
+          .readLineSync()!); // insert value to the variable numb2 from user
+      print(numb1 / numb2); // print the result of the operation to the user
     }
   } else {
-    print("ente a valid operation");
+    // if the user insert an input out of choices print notation statement
+
+    print("ente a valid operation"); // print statement to the user
   }
-  print("thanqu for using our calculator");
+  print("thanqu for using our calculator"); // print statement to the user
 }
 
 double calc(double numb1, double numb2, String operator) {
-  double result = 0;
+  double result = 0; // define a var and give initial value zero
   if (operator == "+") {
-    print(result = numb1 + numb2);
+    // if statement to comparing the operation which the user need and calculate the operation with the relevant class
+
+    print(result =
+        numb1 + numb2); // print the result of the operation to the user
   } else if (operator == "-") {
-    print(result = numb1 - numb2);
+    // if statement to comparing the operation which the user need and calculate the operation with the relevant class
+
+    print(result =
+        numb1 - numb2); // print the result of the operation to the user
   } else if (operator == "*") {
-    print(result = numb1 * numb2);
+    // if statement to comparing the operation which the user need and calculate the operation with the relevant class
+
+    print(result =
+        numb1 * numb2); // print the result of the operation to the user
   } else if (operator == "/") {
-    print(result = numb1 / numb2);
+    // if statement to comparing the operation which the user need and calculate the operation with the relevant class
+
+    print(result =
+        numb1 / numb2); // print the result of the operation to the user
     if (numb2 == 0) {
-      print("the second number should be a natural number");
-      print("enter the second number again");
-      int? numb2 = int.parse(stdin.readLineSync()!);
-      print(result = numb1 / numb2);
+      // conditional statement to avoid divided by zero
+      print(
+          "the second number should be a natural number"); // print statement to the user
+      print("enter the second number again"); // print statement to the user
+      int? numb2 = int.parse(stdin
+          .readLineSync()!); // insert value to the variable numb2 from user
+
+      print(result =
+          numb1 / numb2); // print the result of the operation to the user
     }
   } else {
-    print("ente a valid operation");
+    // if the user insert an input out of choices print notation statement
+
+    print("ente a valid operation"); // print statement to the user
   }
-  return result;
+  return result; // return statement
 }
